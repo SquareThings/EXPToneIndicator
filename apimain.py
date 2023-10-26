4c03c9603fde84c7ff98988c7cf6394b2ab4a4b7
SquareThings <amy_xiao@brown.edu>
2023-10-26 14:57:40 -0400

    Barebones

---
+++ apimain.py
@@ -0,0 +1,10 @@
+import pytumblr
+
+client = pytumblr.TumblrRestClient(
+    '<consumer_key>',
+    '<consumer_secret>',
+    '<oauth_token>',
+    '<oauth_secret>',
+)
+
+client.info() # Grabs the current user information
